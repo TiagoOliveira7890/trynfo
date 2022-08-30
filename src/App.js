e8d5f7d1897a0839a2e4499c03c0995028d8a2e9
Tiago Oliveira <pliveiratiago7890@gmail.com>
2022-08-30 08:52:07 -0300

    Ajustando erros

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Form from './components/Form';
-import Card from './components/Card';
 
 class App extends React.Component {
   render() {
@@ -8,7 +7,6 @@ class App extends React.Component {
       <div>
         <h1>Tryunfo</h1>
         <Form />
-        <Card />
       </div>
     );
   }
